1c3e9f953b2a276009be7b4e6e658cb967b2af8a
Kamlesh Kumar <dev.slinfy@gmail.com>
2022-07-16 13:09:48 +0530

    Kamlesh

--- lib/src/my_app.dart
+++ lib/src/my_app.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:get/route_manager.dart';
+import 'package:slinfy_crm/src/ui/auth/login.dart';
 import 'package:slinfy_crm/src/ui/home/home.dart';
+import 'package:slinfy_crm/src/ui/splash/splash.dart';
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      // initialRoute: "/",
       debugShowCheckedModeBanner: false,
-      home: Home(),
+      home: Login(),
     );
   }
 }
---
+++ lib/src/ui/splash/splash.dart
@@ -0,0 +1,32 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:slinfy_crm/src/ui/auth/login.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({Key? key}) : super(key: key);
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  initState() {
+    Timer(
+        const Duration(seconds: 3),
+        () => Navigator.of(context)
+            .pushReplacement(MaterialPageRoute(builder: (context) => Login())));
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image.network(
+            'https://d13dtqinv406lk.cloudfront.net/company/images/4191.png'),
+      ),
+    );
+  }
+}
